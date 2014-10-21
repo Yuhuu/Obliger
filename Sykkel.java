7fda6acb63d9138e382d9d5c386d751d3eaa3a8f
yuhuu <vicky.huangyuanxin@gmail.com>
2014-10-21 08:09:03 +0200

    hello

--- Sykkel.java
+++ Sykkel.java
@@ -1,7 +1,8 @@
 package Obliger;
 
 /**
- * Created by yuanxin huang 21.10.14 s184519.
+ * Created by yuanxin huang 21.10.14 s184519.this is goood
  */
 public class Sykkel {
+    public int a = 9;
 }
